beba1caf30b7ec4358126aa318a24d51036be112
GauthamAjayKannan <43108156+GauthamAjayKannan@users.noreply.github.com>
2019-01-27 09:28:44 +0530

    Update romnum.py

--- romnum.py
+++ romnum.py
@@ -1,5 +1,6 @@
 #numrom
-n=list(input())
+n=input()
 rom=['I','II','III','IV','V','VI','VII','VIII','IX','X']
-n=[rom.index(i)+1 for i in n if i in rom]
-print(sum(n))
+n=n if n in rom else list(n)
+n=rom.index(n)+1 if isinstance(n,str) else sum([rom.index(i)+1 for i in n if i in rom])
+print(n)
