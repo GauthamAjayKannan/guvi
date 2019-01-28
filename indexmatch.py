03ad2520ca0af7f98efad0c1ed2923d3ca8388c6
GauthamAjayKannan <43108156+GauthamAjayKannan@users.noreply.github.com>
2019-01-28 17:03:29 +0530

    Update indexmatch.py

--- indexmatch.py
+++ indexmatch.py
@@ -1,6 +1,10 @@
+# your code goes here
 #indexmatch
 n=input()
 l=list(map(int,input().split(" ")))
 t=enumerate(l)
 l=[i[0] for i in t if i[0]==i[1]]
-print(*l if l!=[] else "-1")
+if l==[]:
+	print(-1)
+else:
+	print(*l)
