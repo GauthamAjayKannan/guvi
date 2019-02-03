4564d0320ac4588e66eb473ee43d0bf15dab707d
GauthamAjayKannan <43108156+GauthamAjayKannan@users.noreply.github.com>
2019-02-03 21:14:32 +0530

    Update commonint.py

--- commonint.py
+++ commonint.py
@@ -1,6 +1,9 @@
-# your code goes here
+#commonint
 n=input()
 l1=input().split(" ")
 l2=input().split(" ")
-r=[i for i in l1 if i in l2]
+r=[]
+for i in l1:
+	if i in l2 and i not in r:
+		r.append(i)
 print(*r)
