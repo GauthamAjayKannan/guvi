3ca609068dc9e37307bef0ada6df3ef25a8d0714
GauthamAjayKannan <43108156+GauthamAjayKannan@users.noreply.github.com>
2019-03-13 16:47:45 +0530

    Update incr.py

--- incr.py
+++ incr.py
@@ -1,9 +1,14 @@
 n=int(input())
 l=list(map(int,input().split(" ")))
 r=1
-for i in range(len(l)):
+res=0
+for i in range(len(l)-1):
 	if l[i]<l[i+1]:
 		r=r+1
 	else:
-		print(r)
-		break
+		if res<r:
+			res=r
+		r=1
+if res<r:
+	res=r
+print(res)
