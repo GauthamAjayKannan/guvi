499d79ce64f058390a1135552554125c5ce5e0c0
GauthamAjayKannan <43108156+GauthamAjayKannan@users.noreply.github.com>
2019-05-05 15:12:08 +0530

    Rename substring.py to substn.py

---
+++ substn.py
@@ -0,0 +1,18 @@
+s1=input()
+s2=input()
+k=False
+for i in range(len(s1)):
+	a=i
+	l=0
+	for j in range(len(s2)):
+		if s1[a]==s2[j]:
+			l=l+1
+			a=a+1
+		else:
+			break
+	if len(s2)==l:
+		print(i)
+		k=True
+		break
+if k==False:
+	print(-1)
