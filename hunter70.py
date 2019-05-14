3dcb469b398cd822433df94952f6175e3d74ae8f
GauthamAjayKannan <43108156+GauthamAjayKannan@users.noreply.github.com>
2019-05-14 14:04:47 +0530

    Update hunter70.py

--- hunter70.py
+++ hunter70.py
@@ -4,11 +4,11 @@ coun=0
 for i in range(len(l)-1):
 	for j in range(i+1,len(l)):
 		if l[i]+l[j]==t:
-			print("yes")
+			print("YES")
 			coun=1
 			break
 	if coun==1:
 		break
 		
 if coun==0:
-	print("no")
+	print("NO")
