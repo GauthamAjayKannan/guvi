3fe5e09e20d6bced19cf9c61c15ff3ed5c63bebc
GauthamAjayKannan <43108156+GauthamAjayKannan@users.noreply.github.com>
2019-05-14 16:53:10 +0530

    Update hunter69.py

--- hunter69.py
+++ hunter69.py
@@ -4,7 +4,7 @@ if '@' not in s:
 	che=False
 if s.count('@')>1 or s.count('.')>1 and che==True:
 	che=False
-if len(s[s.index('@'):s.index('.')+1])<4 and che==True:
+if len(s[s.index('@')+1:s.index('.')])<4 or s[s.index('@')+1:s.index('.')]!="gmail" and che==True:
 	che=False
 if len(s[:s.index('@')])<3 and che==True:
 	che=False
