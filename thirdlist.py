3c3fd0e5fd57daf804c4da0979f8d3416d341466
GauthamAjayKannan <43108156+GauthamAjayKannan@users.noreply.github.com>
2019-05-14 13:14:41 +0530

    Update thirdlist.py

--- thirdlist.py
+++ thirdlist.py
@@ -1,5 +1,7 @@
 n=input()
 l1=list(map(int,input().split(" ")))
 l2=list(map(int,input().split(" ")))
-r=[l1[i]+l2[i] for i in range(len(l1))]
+r=[]
+for i in range(len(l1)):
+	r.append(l1[i]+l2[i])
 print(*r)
