e10f932e6b2681f5a2ec59316f8b384b939e38ea
GauthamAjayKannan <43108156+GauthamAjayKannan@users.noreply.github.com>
2019-05-15 12:03:26 +0530

    Update consedelete.py

--- consedelete.py
+++ consedelete.py
@@ -1,7 +1,9 @@
-n=input()
-n=list(n[::-1])
+n=list(input())
+r=0
 for i in range(len(n)-1):
+	t=n.copy()
 	if n[i]==n[i+1]:
-		del n[i]
-		break
-print("".join(n[::-1]))
+		t.pop(i)
+		if r<int("".join(t)):
+			r=int("".join(t))
+print(r)
