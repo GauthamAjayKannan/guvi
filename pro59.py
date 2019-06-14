adc33f2a85390f51cdd1fcc73810160f84c9ca47
GauthamAjayKannan <43108156+GauthamAjayKannan@users.noreply.github.com>
2019-06-14 09:10:28 +0530

    Rename pro44.py to pro59.py

---
+++ pro59.py
@@ -0,0 +1,8 @@
+s=input()
+inp=input()
+if len(s[:s.index('|')]+inp)==len(s[s.index('|')+1:]):
+	print(s[:s.index('|')]+inp+s[s.index('|')+1:])
+elif len(s[:s.index('|')])==len(s[s.index('|')+1:]+inp):
+	print(s+inp)
+else:
+	print("Impossible")
