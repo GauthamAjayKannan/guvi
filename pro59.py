9c980283a2757cfae0153dd9a69f60645e63a4b4
GauthamAjayKannan <43108156+GauthamAjayKannan@users.noreply.github.com>
2019-06-14 09:21:44 +0530

    Update pro59.py

--- pro59.py
+++ pro59.py
@@ -1,7 +1,7 @@
 s=input()
 inp=input()
 if len(s[:s.index('|')]+inp)==len(s[s.index('|')+1:]):
-	print(s[:s.index('|')+1]+inp+s[s.index('|')+1:])
+	print(s[:s.index('|')]+inp+"|"+s[s.index('|')+1:])
 elif len(s[:s.index('|')])==len(s[s.index('|')+1:]+inp):
 	print(s+inp)
 else:
